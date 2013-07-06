426aa4de8c7a588b059b32dc5c4d0d373fd9dc1f
fahad <fahadghanidgp@gmail.com>
2013-07-06 15:58:51 -0700

    Initial commit

---
+++ link-finder/crawler.py
@@ -0,0 +1,53 @@
+'''
+Created on Jul 6, 2013
+
+@author: fahad
+'''
+
+import utils
+
+class Crawler(object):
+    
+    def __init__(self, url):
+        self.base_url = url
+        self.visited = {}
+        self.all_urls = []
+        self.count = 0
+        self.queue = []
+        
+        self.queue.insert(0, self.base_url)
+        
+        
+    def run(self):
+        
+        while self.queue:
+            
+            url = self.queue.pop()
+
+            # proceed only if this url is not visited yet
+            if not self.visited.get(url, False):
+                
+                try:
+                    list_of_urls = utils.find_links(url)
+                except Exception as e:
+                    print e
+                    list_of_urls = []
+                
+                for u in list_of_urls:
+                    full_url = self.base_url + u
+                    if not self.visited.get(full_url, False):
+                        self.queue.insert(0, full_url)
+                    
+                self.visited[url] = True
+                self.count += 1
+
+
+if __name__ == '__main__':
+    crawler = Crawler(url='http://still-beyond-8192.herokuapp.com/')
+    crawler.run()
+    print crawler.count
+    
+    with open('all_links', 'w') as f:
+        for k,v in crawler.visited.iteritems():
+            f.write(k + '\n')
+    
---
+++ link-finder/utils.py
@@ -0,0 +1,53 @@
+'''
+Created on Jul 6, 2013
+
+@author: fahad
+'''
+import urllib2
+import re
+
+
+def get_pattern():
+    """
+    This pattern is insufficient to capture *ALL* the possible cases we may have in the html
+    This pattern returns all relative urls on the page
+    what about absolute urls? 
+    """
+    pattern = 'href="/(.*)/"'
+    return pattern
+
+
+def send_and_receive(url):
+    req = urllib2.Request(url)
+
+    resp = urllib2.urlopen(req, timeout=30)
+    
+    # retry if response code wasn't 2xx, 4xx or 5xx
+    page_data = resp.read()
+    return page_data
+        
+    
+def find_links(url):
+    """
+    url = 'http://something.com/'
+    url = 'http://something.com/download'
+    url = 'http://something.com/documentation'
+    """
+#    if base_url:
+#        full_url = base_url + url
+#    else:
+#        full_url = url
+    
+    page_data = send_and_receive(url)
+    urls_on_this_page = re.findall(get_pattern(), page_data)
+    return urls_on_this_page
+
+
+if __name__ == '__main__':
+    url = 'http://still-beyond-8192.herokuapp.com/'
+    links = find_links(url)
+    print links
+    
+    
+    
+    
